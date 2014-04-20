676aa311e18e7b5acff8a8088910f8e326d95b75
serge304 <kazachenko.sergey@gmail.com>
2014-04-19 22:51:57 -0400

    Update GPUArray.cuh

--- GPUArray.cuh
+++ GPUArray.cuh
@@ -5,7 +5,7 @@ Last modified: April 10, 2014
 */
    
 /*! \file GPUArray.cuh
-    \brief Defines the DeviceArray and HostDeviceArray classes
+    \brief Defines the DeviceArray and DeviceHostArray classes
 */
 
 #define NDEBUG 
